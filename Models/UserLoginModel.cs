0fd86c4ff26d06c7ca5197ba0bd01f1976382641
Nobir <nobirfreelancer@gmail.com>
2022-07-16 06:30:43 +0600

    [Added] login system and controller

---
+++ Models/UserLoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ERPSystemTimologio.EF;
+
+namespace ERPSystemTimologio.Models
+{
+    public class UserLoginModel: User
+    {
+        public UserLoginModel()
+        {
+
+        }
+
+        [Required, EmailAddress]
+        public new string Email { get; set; }
+        [Required]
+        public new string Password { get; set; }
+    }
+}
