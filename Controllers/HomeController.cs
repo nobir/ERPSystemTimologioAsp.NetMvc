acc26859b5869b036676f42d411375b7ba372ee3
Nobir <nobirfreelancer@gmail.com>
2022-07-16 06:30:02 +0600

    [Added] login system and controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ERPSystemTimologio.Auth;
+using ERPSystemTimologio.EF;
+using ERPSystemTimologio.Models;
 
 namespace ERPSystemTimologio.Controllers
 {
@@ -11,6 +14,88 @@ namespace ERPSystemTimologio.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet, NotLoggedIn]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserLoginModel user)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(user);
+            }
+
+            var db = new TimologioEntities();
+            var db_user = db.Users.Where(u => u.Email == user.Email).SingleOrDefault();
+
+            if (db_user == null)
+            {
+                TempData["error_message"] = "Invalid email or password";
+                return RedirectToAction("Login", "Home");
+            }
+
+            bool verified_check = db_user.Verified == 1;
+
+            if (!verified_check)
+            {
+                TempData["error_message"] = "Your account is not verified yet";
+                return RedirectToAction("Login", "Home");
+            }
+
+            bool password_check = db_user.Password == user.Password;
+
+            if (!password_check)
+            {
+                TempData["error_message"] = "Invalid email or password";
+                return RedirectToAction("Login", "Home");
+            }
+
+            db_user.Status = 1;
+
+            db.WorkingHours.Add(new WorkingHour
+                {
+                    UserId = db_user.Id,
+                    Date = DateTime.Now.Date,
+                    EntryTime = DateTime.Now,
+                }
+            );
+
+            Session["user"] = db_user;
+            Session["loggedin"] = true;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+        public ActionResult Logout()
+        {
+            var sessionUser = (User)Session["user"];
+            if(sessionUser != null)
+            {
+                var db = new TimologioEntities();
+                var db_working_hour = db.WorkingHours.Where(w => w.UserId == sessionUser.Id && w.ExitTime == null).SingleOrDefault();
+
+                if(db_working_hour != null)
+                {
+                    db_working_hour.ExitTime = DateTime.Now;
+                    db.SaveChanges();
+                }
+            }
+
+            Session.RemoveAll();
+
+            return RedirectToAction("Login", "Home");
         }
     }
 }
