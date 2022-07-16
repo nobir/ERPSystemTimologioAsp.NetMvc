92839a993ff876cbe46c34c6cd8f89b6e9591133
Nobir <nobirfreelancer@gmail.com>
2022-07-16 06:31:35 +0600

    [Added] Dashboard pages

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ERPSystemTimologio.Auth;
+
+namespace ERPSystemTimologio.Controllers
+{
+    [LoggedIn]
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
