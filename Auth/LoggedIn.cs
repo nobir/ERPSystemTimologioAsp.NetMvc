74894f1498d5c4f5af6f7eab225b76e127bdfb88
Nobir <nobirfreelancer@gmail.com>
2022-07-16 06:30:22 +0600

    [Added] login system and controller

---
+++ Auth/LoggedIn.cs
@@ -0,0 +1,28 @@
+﻿using ERPSystemTimologio.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ERPSystemTimologio.Auth
+{
+    public class LoggedIn : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            bool sessionLoggedin = filterContext.HttpContext.Session["loggedin"] != null && filterContext.HttpContext.Session["loggedin"].ToString().Equals("True");
+            if (sessionLoggedin != true)
+            {
+                filterContext.Controller.TempData["error_message"] = "You need to login first";
+
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(new { controller = "Home", action = "Login" })
+                );
+
+                filterContext.Result.ExecuteResult(filterContext.Controller.ControllerContext);
+            }
+        }
+    }
+}
---
+++ Auth/NotLoggedIn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ERPSystemTimologio.Auth
+{
+    public class NotLoggedIn : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            bool sessionLoggedin = filterContext.HttpContext.Session["loggedin"] != null && filterContext.HttpContext.Session["loggedin"].ToString().Equals("True");
+            if (sessionLoggedin == true)
+            {
+                filterContext.Controller.TempData["error_message"] = "You are alreay logged in";
+
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(new { controller = "Dashboard", action = "Index" })
+                );
+
+                filterContext.Result.ExecuteResult(filterContext.Controller.ControllerContext);
+            }
+        }
+    }
+}
