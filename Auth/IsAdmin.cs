0e99862d468c8309ed455a02ea60c15efd75dfdd
Nobir <nobirfreelancer@gmail.com>
2022-07-16 06:28:10 +0600

    [Added] admin controller and these actions/methods can only be access by Admin user or higher

---
+++ Auth/IsAdmin.cs
@@ -0,0 +1,34 @@
+﻿using ERPSystemTimologio.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ERPSystemTimologio.Auth
+{
+    public class IsAdmin : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            User sessionUser = null;
+
+            if(filterContext.HttpContext.Session["user"] != null)
+            {
+                sessionUser = (User)filterContext.HttpContext.Session["user"];
+            }
+
+            if (!(sessionUser != null && sessionUser.Type <= 1))
+            {
+                filterContext.Controller.TempData["error_message"] = "Your not Admin or System User";
+
+                filterContext.Result = new RedirectToRouteResult(
+                    new RouteValueDictionary(new { controller = "Dashboard", action = "Index" })
+                );
+
+                filterContext.Result.ExecuteResult(filterContext.Controller.ControllerContext);
+            }
+        }
+    }
+}
---
+++ Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using ERPSystemTimologio.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERPSystemTimologio.Controllers
+{
+    [IsAdmin]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
