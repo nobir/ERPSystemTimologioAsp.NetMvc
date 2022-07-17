04e417b04c5c4a5361bfe946bfdd459810aba899
Nobir <nobirfreelancer@gmail.com>
2022-07-18 05:00:34 +0600

    [Added] check email and username unique validation

---
+++ Models/UniqueEmailValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ERPSystemTimologio.EF;
+
+namespace ERPSystemTimologio.Models
+{
+    public class UniqueEmailValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if(value != null)
+            {
+                var db = new TimologioEntities();
+
+                int isExists = db.Users.Where(u => u.Email.Equals(value.ToString())).Count();
+                if (isExists > 0)
+                {
+                    return new ValidationResult("The email is already is registered");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Models/UniqueUsernameValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ERPSystemTimologio.EF;
+
+namespace ERPSystemTimologio.Models
+{
+    public class UniqueUsernameValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+
+            if(value != null)
+            {
+                var db = new TimologioEntities();
+
+                int isExists = db.Users.Where(u => u.Username.Equals(value.ToString())).Count();
+                if (isExists > 0)
+                {
+                    return new ValidationResult("The username is already is registered");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
